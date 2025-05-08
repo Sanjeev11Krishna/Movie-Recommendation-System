77a1cde0a0bbab2f4f0fd523c53aed91a05a831f
sri2006-ops <jayanthasrim@gmail.com>
2025-05-08 22:54:24 +0530

    Update Login.jsx

--- FrontEnd/src/pages/Login/Login.jsx
+++ FrontEnd/src/pages/Login/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Login.css'
+import logo from '/Users/kumaranjayaraman/Desktop/netflix clone/src/assets/logo.png'
 const Login = () => {
   return (
     <div className='login'>
-      
+        <img src={logo}  className='login-logo' alt=""/>
+      <div className="login-form">
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" placeholder='your name'/>
+        <input type="email" placeholder='email'/>
+        <input type="password" placeholder='password'/>
+        <button>Sign In</button>
+        <div className="form-help">
+            <div className='remember'>
+                <input type="chechbox"/>
+                <label htmlForm="">Remember Me</label>
+            </div>
+        <p>Need Help?</p>
+        </div>
+      </form>  
+      <div className="form-switch">
+        <p>New to Netflix? <span>Sign Up Now</span></p>
+        <p>Already HAve Account? <span>Sign In Now</span></p>
+
+      </div>
+      </div>
     </div>
   )
 }
