91bff86bab984f860d9c0bfbe96372bb04912988
sri2006-ops <jayanthasrim@gmail.com>
2025-05-11 16:25:42 +0530

    updated frontend with firbase authentication

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import Home from './pages/home/Home';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Player from './pages/Player/Player';
+import Login from './pages/Login/Login';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // ✅ Import Toastify styles
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("logged In");
+        navigate('/');
+      } else {
+        console.log("logged Out");
+        navigate('/login');
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
